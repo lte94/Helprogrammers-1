0cb9f3850722bcfe8fe913b9498d62ba761b299a
ehfdl <cmaw@naver.com>
2022-12-26 22:53:02 +0900

    🔥 remove(search)
    searchget 삭제, filter 사용
    다영 승효 영진 merge
    
    by @ehfdl

--- src/redux/module/QuestionsSlice.jsx
+++ src/redux/module/QuestionsSlice.jsx
@@ -69,19 +69,6 @@ export const questionsSlice = createSlice({
       state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
       state.error = action.payload; // catch 된 error 객체를 state.error에 넣습니다.
     },
-
-    [__getSearchedQuestions.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [__getSearchedQuestions.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.questions = action.payload;
-    },
-    [__getSearchedQuestions.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-
     [__getQuestion.pending]: (state) => {
       state.isLoading = true; // 네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
     },
